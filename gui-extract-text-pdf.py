713f5fa433efb4bd167f3c30dcc4b968dff0eb7e
someoneelse <someoneelse@mail.com>
2023-05-07 01:54:42 -0300

    done

--- gui-extract-text-pdf.py
+++ gui-extract-text-pdf.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+#-*- coding:utf8 -*-
 
-import PyPDF2 as ppdf
-import tkinter as tk
+import PyPDF2 as pdf, tkinter as tk
+from tkinter import filedialog
 
 t=tk.Tk()
 t.title("Text extractor from pdf")
@@ -9,17 +10,12 @@ t.title("Text extractor from pdf")
 fpdf=tk.StringVar()
 wp=tk.StringVar()
 
-def p(fn,w):
-    fn=fn+".pdf"
-    o=open(fn,"rb")
-    p=ppdf.PdfFileReader(o);
-    i=p.numPages
-    oo=open(w,"w")
-    for x in range(0,i):
-        xd=p.getPage(x)
-        oo.write(xd.extractText())
-    oo.close()
-    print("Writted")
+def p(fn,wp):
+	p=pdf.PdfReader(fn+".pdf")
+	oo=open(wp,"w")
+	for i in p.pages:
+		oo.write(i.extract_text())
+	
 
 tl0=tk.Label(t,text="Portable File Format (pdf) to read: ")
 tl0.grid(row=0,column=0)
@@ -27,7 +23,7 @@ te0=tk.Entry(t,textvariable=fpdf)
 te0.grid(row=0,column=1)
 l0=tk.Label(t,text="(without extension)")
 l0.grid(row=1,column=1)
-tl1=tk.Label(t,text="Text filename to write: ")
+tl1=tk.Label(t,text="Filename to write: ")
 tl1.grid(row=2,column=0)
 te1=tk.Entry(t,textvariable=wp)
 te1.grid(row=2,column=1)
